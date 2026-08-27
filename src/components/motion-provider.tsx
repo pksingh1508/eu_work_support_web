2127793fcfb1354e7e88da7caaa07523a3933727
Pawan Kumar <rajiit1212@gmail.com>
2026-08-28 00:10:22 +0530

    add motion provider

--- src/components/motion-provider.tsx
+++ src/components/motion-provider.tsx
@@ -1,10 +1,10 @@
 "use client";
 
-import { domAnimation, LazyMotion, MotionConfig } from "motion/react";
+import { domMax, LazyMotion, MotionConfig } from "motion/react";
 
 export function MotionProvider({ children }: { children: React.ReactNode }) {
   return (
-    <LazyMotion features={domAnimation} strict>
+    <LazyMotion features={domMax} strict>
       <MotionConfig reducedMotion="user">{children}</MotionConfig>
     </LazyMotion>
   );
